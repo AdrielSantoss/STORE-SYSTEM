4bc62caf3775cf726fbbc1738ddbbbdde50f59ac
Adriel dos Santos <adrieldossantos7@gmail.com>
2023-01-12 19:52:19 -0300

    Remove classes e configurações padrões de criação de web api em .net

--- backend/StoreSystem/Program.cs
+++ backend/StoreSystem/Program.cs
@@ -11,10 +11,7 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+{ }
 
 app.UseAuthorization();
 
